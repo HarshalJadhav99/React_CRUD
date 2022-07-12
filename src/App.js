fb3b6a94e0209f5d0c2c5ee094fa244f72547b78
harshal_jadhav <harshal.jadhav@galepartners.com>
2022-07-12 13:53:37 +0530

    searching_sorting -update app.js file.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/pages/Home";
 import AddUser from "./components/users/AddUser";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import EditUser from "./components/users/EditUser";
 import User from "./components/users/User";
 
 function App() {
 	return (
 		<div>
-			<Router>
+			<Router basename={window.location.pathname || ''}>
 					
 				<Routes>
 					<Route exact path="/" element={<Home />} />
