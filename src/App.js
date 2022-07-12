adca24a5cdda2a02225405464e2a4a38184797cc
harshal_jadhav <harshal.jadhav@galepartners.com>
2022-07-12 13:36:54 +0530

    searching_sorting -update the package.json file.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/pages/Home";
 import AddUser from "./components/users/AddUser";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import EditUser from "./components/users/EditUser";
 import User from "./components/users/User";
 
